e23880fbf5975ed57da23122132d0714f3446a94
Jonathan Caley <jonathan.caley@ocado.com>
2019-11-03 17:34:55 +0000

    Cleanup

--- app/src/main/java/com/example/cryptomanager_v2/data/model/ExchangeRates/ExchangeRatesOld.kt
+++ app/src/main/java/com/example/cryptomanager_v2/data/model/ExchangeRates/ExchangeRatesOld.kt
@@ -2,14 +2,15 @@ package com.example.cryptomanager_v2.data.model.ExchangeRates
 
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
+import com.google.gson.JsonElement
+import com.google.gson.JsonArray
+import com.google.gson.JsonObject
 
 class ExchangeRatesOld {
 
     @SerializedName("base")
-    @Expose
     var base: String? = null
 
     @SerializedName("rates")
-    @Expose
     var rates: RatesOld? = null
 }
--- app/src/main/java/com/example/cryptomanager_v2/data/model/ExchangeRates/RatesOld.kt
+++ app/src/main/java/com/example/cryptomanager_v2/data/model/ExchangeRates/RatesOld.kt
@@ -1,108 +1,106 @@
 package com.example.cryptomanager_v2.data.model.ExchangeRates
 
-import com.google.gson.annotations.Expose
+//import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 class RatesOld {
 
     @SerializedName("BGN")
-    @Expose
     var bgn: Double? = null
-    @SerializedName("NZD")
-    @Expose
-    var nzd: Double? = null
-    @SerializedName("ILS")
-    @Expose
-    var ils: Double? = null
-    @SerializedName("RUB")
-    @Expose
-    var rub: Double? = null
-    @SerializedName("CAD")
-    @Expose
-    var cad: Double? = null
-    @SerializedName("USD")
-    @Expose
-    var usd: Double? = null
-    @SerializedName("PHP")
-    @Expose
-    var php: Double? = null
-    @SerializedName("CHF")
-    @Expose
-    var chf: Double? = null
-    @SerializedName("AUD")
-    @Expose
-    var aud: Double? = null
-    @SerializedName("JPY")
-    @Expose
-    var jpy: Double? = null
-    @SerializedName("TRY")
-    @Expose
-    var `try`: Double? = null
-    @SerializedName("HKD")
-    @Expose
-    var hkd: Double? = null
-    @SerializedName("MYR")
-    @Expose
-    var myr: Double? = null
-    @SerializedName("HRK")
-    @Expose
-    var hrk: Double? = null
-    @SerializedName("CZK")
-    @Expose
-    var czk: Double? = null
-    @SerializedName("IDR")
-    @Expose
-    var idr: Double? = null
-    @SerializedName("DKK")
-    @Expose
-    var dkk: Double? = null
-    @SerializedName("NOK")
-    @Expose
-    var nok: Double? = null
-    @SerializedName("HUF")
-    @Expose
-    var huf: Double? = null
-    @SerializedName("GBP")
-    @Expose
-    var gbp: Double? = null
-    @SerializedName("MXN")
-    @Expose
-    var mxn: Double? = null
-    @SerializedName("THB")
-    @Expose
-    var thb: Double? = null
-    @SerializedName("ISK")
-    @Expose
-    var isk: Double? = null
-    @SerializedName("ZAR")
-    @Expose
-    var zar: Double? = null
-    @SerializedName("BRL")
-    @Expose
-    var brl: Double? = null
-    @SerializedName("SGD")
-    @Expose
-    var sgd: Double? = null
-    @SerializedName("PLN")
-    @Expose
-    var pln: Double? = null
-    @SerializedName("INR")
-    @Expose
-    var inr: Double? = null
-    @SerializedName("KRW")
-    @Expose
-    var krw: Double? = null
-    @SerializedName("RON")
-    @Expose
-    var ron: Double? = null
-    @SerializedName("CNY")
-    @Expose
-    var cny: Double? = null
-    @SerializedName("SEK")
-    @Expose
-    var sek: Double? = null
-    @SerializedName("EUR")
-    @Expose
-    var eur: Double? = null
 
+    @SerializedName("NZD")
+    var nzd: Double? = null
+
+    @SerializedName("ILS")
+    var ils: Double? = null
+
+    @SerializedName("RUB")
+    var rub: Double? = null
+
+    @SerializedName("CAD")
+    var cad: Double? = null
+
+    @SerializedName("USD")
+    var usd: Double? = null
+
+    @SerializedName("PHP")
+    var php: Double? = null
+
+    @SerializedName("CHF")
+    var chf: Double? = null
+
+    @SerializedName("AUD")
+    var aud: Double? = null
+
+    @SerializedName("JPY")
+    var jpy: Double? = null
+
+    @SerializedName("TRY")
+    var `try`: Double? = null
+
+    @SerializedName("HKD")
+    var hkd: Double? = null
+
+    @SerializedName("MYR")
+    var myr: Double? = null
+
+    @SerializedName("HRK")
+    var hrk: Double? = null
+
+    @SerializedName("CZK")
+    var czk: Double? = null
+
+    @SerializedName("IDR")
+    var idr: Double? = null
+
+    @SerializedName("DKK")
+    var dkk: Double? = null
+
+    @SerializedName("NOK")
+    var nok: Double? = null
+
+    @SerializedName("HUF")
+    var huf: Double? = null
+
+    @SerializedName("GBP")
+    var gbp: Double? = null
+
+    @SerializedName("MXN")
+    var mxn: Double? = null
+
+    @SerializedName("THB")
+    var thb: Double? = null
+
+    @SerializedName("ISK")
+    var isk: Double? = null
+
+    @SerializedName("ZAR")
+    var zar: Double? = null
+
+    @SerializedName("BRL")
+    var brl: Double? = null
+
+    @SerializedName("SGD")
+    var sgd: Double? = null
+
+    @SerializedName("PLN")
+    var pln: Double? = null
+
+    @SerializedName("INR")
+    var inr: Double? = null
+
+    @SerializedName("KRW")
+    var krw: Double? = null
+
+    @SerializedName("RON")
+    var ron: Double? = null
+
+    @SerializedName("CNY")
+    var cny: Double? = null
+
+    @SerializedName("SEK")
+    var sek: Double? = null
+
+    @SerializedName("EUR")
+    var eur: Double? = null
 }
