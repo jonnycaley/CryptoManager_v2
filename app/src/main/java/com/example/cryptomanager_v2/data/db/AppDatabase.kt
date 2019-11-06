3e264c9e05990ea08fbfa3801bade2f0a3b2db44
Jonathan Caley <jonathan.caley@ocado.com>
2019-11-06 21:48:20 +0000

    Add crypto dao

--- app/src/main/java/com/example/cryptomanager_v2/data/db/AppDatabase.kt
+++ app/src/main/java/com/example/cryptomanager_v2/data/db/AppDatabase.kt
@@ -2,8 +2,13 @@ package com.example.cryptomanager_v2.data.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.cryptomanager_v2.data.db.cryptos.DBCrypto
+import com.example.cryptomanager_v2.data.db.cryptos.DBCryptosDao
+import com.example.cryptomanager_v2.data.db.fiats.DBFiat
+import com.example.cryptomanager_v2.data.db.fiats.DBFiatsDao
 
-@Database(entities = [DBFiat::class], version = 1, exportSchema = false)
-abstract class AppDatabase : RoomDatabase() {
-    abstract fun fiatsDao() : DBFiatsDao
+@Database(entities = [DBFiat::class, DBCrypto::class], version = 1, exportSchema = false)
+abstract class AppDatabase: RoomDatabase() {
+    abstract fun fiatsDao(): DBFiatsDao
+    abstract fun cryptosDao(): DBCryptosDao
 }
