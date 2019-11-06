68ea87739559b50ee262b6c778ffde21241d16c8
Jonathan Caley <jonathan.caley@ocado.com>
2019-11-06 21:50:41 +0000

    VM additions

--- app/src/main/java/com/example/cryptomanager_v2/utils/Constants.kt
+++ app/src/main/java/com/example/cryptomanager_v2/utils/Constants.kt
@@ -2,4 +2,9 @@ package com.example.cryptomanager_v2.utils
 
 object Constants {
     val URL_EXCHANGE_RATES = "https://api.exchangeratesapi.io/"
+
+
+    val URL_CRYPTOCOMPARE = "https://min-api.cryptocompare.com/data/"
+    val NAME_CRYPTOCOMPARE = "api_key"
+    val KEY_CRYPTOCOMPARE = "a5df989c3d35841e7434d865f1a818d6b9de1491aae9895022c984833bbfcf00"
 }
--- app/src/main/java/com/example/cryptomanager_v2/utils/di/subcomponents/splash/SplashActivityModule.kt
+++ app/src/main/java/com/example/cryptomanager_v2/utils/di/subcomponents/splash/SplashActivityModule.kt
@@ -1,7 +1,8 @@
 package com.example.cryptomanager_v2.utils.di.subcomponents.splash
 
-import com.example.cryptomanager_v2.data.ExchangeRatesApi
+import com.example.cryptomanager_v2.data.network.ExchangeRatesApi
 import com.example.cryptomanager_v2.data.db.AppDatabase
+import com.example.cryptomanager_v2.data.network.CryptoCompareApi
 import com.example.cryptomanager_v2.ui.splash.SplashViewModelFactory
 import com.example.cryptomanager_v2.utils.di.AppSchedulers
 import com.example.cryptomanager_v2.utils.di.scopes.ActivityScope
@@ -18,13 +19,15 @@ class SplashActivityModule {
         exchangeRatesApi: ExchangeRatesApi,
         schedulers: AppSchedulers,
         database: AppDatabase,
-        gson: Gson
+        gson: Gson,
+        cryptoCompareApi: CryptoCompareApi
     ) : SplashViewModelFactory {
         return SplashViewModelFactory(
             exchangeRatesApi,
             schedulers,
             database,
-            gson
+            gson,
+            cryptoCompareApi
         )
     }
 }
