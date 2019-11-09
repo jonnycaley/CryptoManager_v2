edfb866610212eeae2fcac5ef1dab97e776d61d9
Jonathan Caley <jonathan.caley@ocado.com>
2019-11-09 21:49:50 +0000

    States modified to be called without parameters

--- app/src/main/java/com/example/cryptomanager_v2/utils/Resource.kt
+++ app/src/main/java/com/example/cryptomanager_v2/utils/Resource.kt
@@ -2,16 +2,19 @@ package com.example.cryptomanager_v2.utils
 
 data class Resource<out T>(val status: Status, val data: T?, val message: String?) {
     companion object {
-        fun <T> success(data: T?): Resource<T> {
+        fun <T> success(data: T? = null): Resource<T> {
             return Resource(Status.SUCCESS, data, null)
         }
 
-        fun <T> error(msg: String, data: T?): Resource<T> {
-            return Resource(Status.ERROR, data, msg)
+        fun <T> error(msg: String?): Resource<T> {
+            return Resource(Status.ERROR, null, msg)
         }
 
-        fun <T> loading(data: T?): Resource<T> {
-            return Resource(Status.LOADING, data, null)
+        fun <T> loading(): Resource<T> {
+            return Resource(Status.LOADING, null, null)
+        }
+        fun <T> idle(): Resource<T> {
+             return Resource(Status.IDLE, null, null)
         }
     }
 }
@@ -19,5 +22,6 @@ data class Resource<out T>(val status: Status, val data: T?, val message: String
 enum class Status {
     SUCCESS,
     ERROR,
-    LOADING
+    LOADING,
+    IDLE
 }
