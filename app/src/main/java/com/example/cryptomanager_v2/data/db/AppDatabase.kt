e411e60348f5ba64bf97925330d0cc2eafa9e652
Jonathan Caley <jonathan.caley@ocado.com>
2019-11-09 21:50:29 +0000

    Exchanges added to database and converter added

--- app/src/main/java/com/example/cryptomanager_v2/data/db/AppDatabase.kt
+++ app/src/main/java/com/example/cryptomanager_v2/data/db/AppDatabase.kt
@@ -2,13 +2,19 @@ package com.example.cryptomanager_v2.data.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.example.cryptomanager_v2.data.db.cryptos.DBCrypto
 import com.example.cryptomanager_v2.data.db.cryptos.DBCryptosDao
+import com.example.cryptomanager_v2.data.db.exchanges.CryptosTypeConverter
+import com.example.cryptomanager_v2.data.db.exchanges.DBExchange
+import com.example.cryptomanager_v2.data.db.exchanges.DBExchangeDao
 import com.example.cryptomanager_v2.data.db.fiats.DBFiat
 import com.example.cryptomanager_v2.data.db.fiats.DBFiatsDao
 
-@Database(entities = [DBFiat::class, DBCrypto::class], version = 1, exportSchema = false)
+@Database(entities = [DBFiat::class, DBCrypto::class, DBExchange::class], version = 1, exportSchema = false)
+@TypeConverters(CryptosTypeConverter::class)
 abstract class AppDatabase: RoomDatabase() {
     abstract fun fiatsDao(): DBFiatsDao
     abstract fun cryptosDao(): DBCryptosDao
+    abstract fun exchangesDao(): DBExchangeDao
 }
