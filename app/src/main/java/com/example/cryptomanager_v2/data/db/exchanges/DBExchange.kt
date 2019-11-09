682d37dde0bd3f35b6b591ca587b2bf3c6387d37
Jonathan Caley <jonathan.caley@ocado.com>
2019-11-09 21:51:54 +0000

    Converter for cryptos in exchange database table added

--- app/src/main/java/com/example/cryptomanager_v2/data/db/exchanges/DBExchange.kt
+++ app/src/main/java/com/example/cryptomanager_v2/data/db/exchanges/DBExchange.kt
@@ -1,11 +1,11 @@
 package com.example.cryptomanager_v2.data.db.exchanges
 
-import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.TypeConverters
 
 @Entity
 data class DBExchange(
     @PrimaryKey val exchangeName: String,
-    @ColumnInfo val cryptos: List<Pair<String, List<String>>>
+    @TypeConverters(CryptosTypeConverter::class) val cryptos: Cryptos?
 )
