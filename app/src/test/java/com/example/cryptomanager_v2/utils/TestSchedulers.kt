03ec21b7ecd119650e26accc9ed955b06a2d84f2
Jonathan Caley <jonathan.caley@ocado.com>
2019-11-10 17:55:58 +0000

    test schedulers were incorrect and did not behave as appropriately as trampoline

--- app/src/test/java/com/example/cryptomanager_v2/utils/TestSchedulers.kt
+++ app/src/test/java/com/example/cryptomanager_v2/utils/TestSchedulers.kt
@@ -1,15 +1,15 @@
 package com.example.cryptomanager_v2.utils
 
 import com.example.cryptomanager_v2.utils.di.AppSchedulers
-import io.reactivex.schedulers.TestScheduler
+import io.reactivex.schedulers.Schedulers
 
 open class TestSchedulers {
     companion object {
         fun get(): AppSchedulers {
             return AppSchedulers(
-                TestScheduler(),
-                TestScheduler(),
-                TestScheduler()
+                Schedulers.trampoline(),
+                Schedulers.trampoline(),
+                Schedulers.trampoline()
             )
         }
     }
