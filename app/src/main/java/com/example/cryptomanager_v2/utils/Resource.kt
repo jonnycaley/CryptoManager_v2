41811bfaa01f0d2e48d9a7ee8ed28a9ad8491a41
Jonathan Caley <jonathan.caley@ocado.com>
2019-11-15 10:35:49 +0000

    convert to sealed class

--- app/src/main/java/com/example/cryptomanager_v2/utils/Resource.kt
+++ app/src/main/java/com/example/cryptomanager_v2/utils/Resource.kt
@@ -19,9 +19,9 @@ data class Resource<out T>(val status: Status, val data: T?, val message: String
     }
 }
 
-enum class Status {
-    SUCCESS,
-    ERROR,
-    LOADING,
-    IDLE
+sealed class Status {
+    object SUCCESS : Status()
+    object ERROR: Status()
+    object LOADING: Status()
+    object IDLE: Status()
 }
