4fd21e3380cfee5173acbcc9b908064dd767ac24
Jonathan Caley <jonathan.caley@ocado.com>
2019-11-18 20:46:04 +0000

    New splash content

--- app/src/main/java/com/example/cryptomanager_v2/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/example/cryptomanager_v2/ui/splash/SplashActivity.kt
@@ -1,14 +1,18 @@
 package com.example.cryptomanager_v2.ui.splash
 
 import android.os.Bundle
+import android.view.View
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.cryptomanager_v2.R
+import com.example.cryptomanager_v2.ui.home.HomeActivity
 import com.example.cryptomanager_v2.utils.Status
 import com.example.cryptomanager_v2.utils.exhaustive
 import dagger.android.support.DaggerAppCompatActivity
+import kotlinx.android.synthetic.main.activity_splash.*
 import javax.inject.Inject
 
+@Suppress("IMPLICIT_CAST_TO_ANY")
 class SplashActivity : DaggerAppCompatActivity() {
 
     @Inject
@@ -27,13 +31,29 @@ class SplashActivity : DaggerAppCompatActivity() {
 
         viewModel.apply {
             status.observe(this@SplashActivity, Observer { status ->
+                text_retry.visibility = View.GONE
+                progress_bar.visibility = View.GONE
                 when(status) {
-                    Status.LOADING -> {}
-                    Status.ERROR -> {}
-                    Status.SUCCESS -> {}
-                    Status.IDLE -> {}
-                }.exhaustive
+                    is Status.LOADING -> {
+                        text_loading.text = "Loading data..."
+                        progress_bar.visibility = View.VISIBLE
+                    }
+                    is Status.ERROR -> {
+                        text_loading.text = status.reason
+                        text_retry.visibility = View.VISIBLE
+                    }
+                    is Status.SUCCESS -> {
+                        text_loading.text = "Success!"
+                        HomeActivity.create(this@SplashActivity)
+                    }
+                    is Status.IDLE -> {
+                        text_loading.text = "Idle..."
+                    }
+                }
             })
+            text_retry.setOnClickListener {
+                viewModel.retry()
+            }
         }
     }
     private fun setupToolbar() {
