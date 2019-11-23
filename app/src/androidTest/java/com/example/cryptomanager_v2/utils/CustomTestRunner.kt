1f327cc74048acff6a7c9a5b73a4467441c76a19
Jonathan Caley <jonathan.caley@ocado.com>
2019-11-23 22:20:57 +0000

    Added custom test runner and application class for UI tests

---
+++ app/src/androidTest/java/com/example/cryptomanager_v2/utils/CustomTestRunner.kt
@@ -0,0 +1,11 @@
+package com.example.cryptomanager_v2.utils
+
+import android.app.Application
+import android.content.Context
+import androidx.test.runner.AndroidJUnitRunner
+
+class CustomTestRunner: AndroidJUnitRunner() {
+    override fun newApplication(cl: ClassLoader?, className: String?, context: Context?): Application {
+        return super.newApplication(cl, TestApp::class.java.name, context)
+    }
+}
