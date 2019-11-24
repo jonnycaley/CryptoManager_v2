af60d5a72f663a42b193c2fd338344a561a3a89d
Jonathan Caley <jonathan.caley@ocado.com>
2019-11-24 15:55:24 +0000

    Due to the custom implementation of TestApp and co. files the @get:Rule was creating the activity before the @Setup TestInjector could provide dispatchingandroidinjector and thus, cause an error. Also, setup can now be made for each test before launching activity https://android.jlelse.eu/how-to-inject-mock-dependencies-into-android-components-using-dagger-androidinjector-e274c8f6a9a6

--- app/src/androidTest/java/com/example/cryptomanager_v2/ui/splash/SplashActivityTest.kt
+++ app/src/androidTest/java/com/example/cryptomanager_v2/ui/splash/SplashActivityTest.kt
@@ -1,24 +1,33 @@
 package com.example.cryptomanager_v2.ui.splash
 
-import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.launchActivity
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.cryptomanager_v2.utils.TestAppModule
+import com.example.cryptomanager_v2.utils.TestInjector
+import com.example.cryptomanager_v2.utils.mocks.api.FakeExchangeRatesApi
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.MockitoAnnotations
 
 @RunWith(AndroidJUnit4::class)
 class SplashActivityTest {
 
-    @get:Rule var activitySenarioRule = ActivityScenarioRule<SplashActivity>(SplashActivity::class.java)
+    lateinit var senario: ActivityScenario<SplashActivity>
+
+    private var exchangeRatesApi: FakeExchangeRatesApi = FakeExchangeRatesApi()
 
     @Before
     fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        TestInjector(TestAppModule(exchangeRatesApi)).inject()
     }
 
     @Test
     fun test() {
-        val senario = activitySenarioRule.scenario
+        senario = launchActivity()
+
         SplashActivityRobot()
             .checkLoadingTextIsIdle()
     }
