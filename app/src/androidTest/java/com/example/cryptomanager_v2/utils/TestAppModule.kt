02b1689ee94bdaedbbfee5c3f7f1b18305293c41
Jonathan Caley <jonathan.caley@ocado.com>
2019-11-24 13:15:31 +0000

    App schedulers

--- app/src/androidTest/java/com/example/cryptomanager_v2/utils/TestAppModule.kt
+++ app/src/androidTest/java/com/example/cryptomanager_v2/utils/TestAppModule.kt
@@ -3,8 +3,8 @@ package com.example.cryptomanager_v2.utils
 import com.example.cryptomanager_v2.data.db.exchanges.DBExchangeDao
 import com.example.cryptomanager_v2.data.network.ExchangeRatesApi
 import com.example.cryptomanager_v2.utils.di.AppSchedulers
-import com.example.cryptomanager_v2.utils.mocks.FakeDBExchangesDao
-import com.example.cryptomanager_v2.utils.mocks.FakeExchangeRatesApi
+import com.example.cryptomanager_v2.utils.mocks.db.FakeDBExchangesDao
+import com.example.cryptomanager_v2.utils.mocks.api.FakeExchangeRatesApi
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
@@ -23,13 +23,13 @@ class TestAppModule(
 
     @Provides
     @Singleton
-    fun providesAppSchedulers(): AppSchedulers {
-        return TestAppSchedulers.get()
+    fun providesDBExchangesDao(): DBExchangeDao {
+        return dBExchangeDao
     }
 
     @Provides
     @Singleton
-    fun providesDBExchangesDao(): DBExchangeDao {
-        return dBExchangeDao
+    fun providesAppSchedulers(): AppSchedulers {
+        return TestAppSchedulers.get()
     }
 }
