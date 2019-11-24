76499551eb6bf6dab0cc3bd6654b29a5b8ccd066
Jonathan Caley <jonathan.caley@ocado.com>
2019-11-24 20:45:35 +0000

    Cleanup

--- app/src/androidTest/java/com/example/cryptomanager_v2/ui/splash/SplashActivityTest.kt
+++ app/src/androidTest/java/com/example/cryptomanager_v2/ui/splash/SplashActivityTest.kt
@@ -1,34 +1,42 @@
 package com.example.cryptomanager_v2.ui.splash
 
 import androidx.test.core.app.ActivityScenario
-import androidx.test.core.app.launchActivity
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.cryptomanager_v2.utils.TestAppModule
 import com.example.cryptomanager_v2.utils.TestInjector
 import com.example.cryptomanager_v2.utils.mocks.api.FakeExchangeRatesApi
+import com.example.cryptomanager_v2.utils.mocks.db.FakeDBFiatsDao
+import io.reactivex.Observable
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mockito
 import org.mockito.MockitoAnnotations
 
 @RunWith(AndroidJUnit4::class)
 class SplashActivityTest {
 
-    lateinit var senario: ActivityScenario<SplashActivity>
+    lateinit var scenario: ActivityScenario<SplashActivity>
 
+    private var dbFiatsDao: FakeDBFiatsDao = FakeDBFiatsDao()
     private var exchangeRatesApi: FakeExchangeRatesApi = FakeExchangeRatesApi()
 
     @Before
     fun setUp() {
         MockitoAnnotations.initMocks(this)
-        TestInjector(TestAppModule(exchangeRatesApi)).inject()
     }
 
     @Test
     fun test() {
-        senario = launchActivity()
+
+        launchActivity()
 
         SplashActivityRobot()
             .checkLoadingTextIsIdle()
+    }
+
+    private fun launchActivity() {
+        TestInjector(TestAppModule()).inject()
+        scenario = androidx.test.core.app.launchActivity()
     }
 }
