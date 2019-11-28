c72606437a9b17e53ad7642c61a7df5b575d54b0
Jonathan Caley <jonathan.caley@ocado.com>
2019-11-28 21:07:12 +0000

    Cleanup

--- app/src/main/java/com/example/cryptomanager_v2/data/network/ExchangeRatesApi.kt
+++ app/src/main/java/com/example/cryptomanager_v2/data/network/ExchangeRatesApi.kt
@@ -1,8 +1,6 @@
 package com.example.cryptomanager_v2.data.network
 
-import com.example.cryptomanager_v2.data.model.ExchangeRates.ExchangeRatesOld
 import io.reactivex.Observable
-import io.reactivex.Single
 import retrofit2.http.GET
 
 interface ExchangeRatesApi {
