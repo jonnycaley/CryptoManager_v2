65ece897efc5b85e201dab028332009c728a4034
Jonathan Caley <jonathan.caley@ocado.com>
2019-12-08 20:53:40 +0000

    Cleanup

--- app/src/main/java/com/example/cryptomanager_v2/ui/home/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/cryptomanager_v2/ui/home/ui/settings/SettingsFragment.kt
@@ -1,23 +1,17 @@
 package com.example.cryptomanager_v2.ui.home.ui.settings
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import com.example.cryptomanager_v2.R
 
 class SettingsFragment : Fragment() {
-    companion object {
-        fun newInstance() = SettingsFragment()
-    }
-
     private lateinit var viewModel: SettingsViewModel
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_settings, container, false)
     }
 
