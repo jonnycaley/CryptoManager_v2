a4179d71fe42ec3b28a28d6927eaaea9917598ef
Jonathan Caley <jonathan.caley@ocado.com>
2019-12-08 19:21:54 +0000

    Correct naming and privatization

--- app/src/androidTest/java/com/example/cryptomanager_v2/data/db/fiats/DBFiatsBuilder.kt
+++ app/src/androidTest/java/com/example/cryptomanager_v2/data/db/fiats/DBFiatsBuilder.kt
@@ -2,13 +2,13 @@ package com.example.cryptomanager_v2.data.db.fiats
 
 object DBFiatsBuilder {
 
-    fun buildDBF(): List<DBFiat> {
+    fun buildDB(): List<DBFiat> {
         return listOf(
             buildDBFiat()
         )
     }
 
-    fun buildDBFiat(): DBFiat {
+    private fun buildDBFiat(): DBFiat {
         return DBFiat(name = "USD", rate = 1.0, isBaseFiat = true)
     }
 }
