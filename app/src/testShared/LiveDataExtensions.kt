dafd75132fe21eba7383ee4f416894f2b026966d
Jonathan Caley <jonathan.caley@ocado.com>
2019-12-09 20:24:10 +0000

    New onetime observer function for tests instead of observeforever

---
+++ app/src/testShared/LiveDataExtensions.kt
@@ -0,0 +1,8 @@
+package com.example.cryptomanager_v2.utils
+
+import androidx.lifecycle.LiveData
+
+fun <T> LiveData<T>.observeOnce(onChangeHandler: (T) -> Unit) {
+    val observer = OneTimeObserver(handler = onChangeHandler)
+    observe(observer, observer)
+}
