6ea5b0d9f035d1a5758f546746ba3de4be85bee4
Jonathan Caley <jonathan.caley@ocado.com>
2020-01-02 22:12:57 +0000

    Add base fiats as livedata and observe from activity

--- app/src/main/java/com/example/cryptomanager_v2/ui/home/ui/settings/SettingsViewModel.kt
+++ app/src/main/java/com/example/cryptomanager_v2/ui/home/ui/settings/SettingsViewModel.kt
@@ -1,7 +1,20 @@
 package com.example.cryptomanager_v2.ui.home.ui.settings
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.example.cryptomanager_v2.data.db.fiats.DBFiat
+import com.example.cryptomanager_v2.data.db.fiats.DBFiatsDao
+import javax.inject.Inject
 
-class SettingsViewModel: ViewModel() {
-    // TODO: Implement the ViewModel
+class SettingsViewModel @Inject constructor(
+    private val fiatsDao: DBFiatsDao
+): ViewModel() {
+
+    val fiats: LiveData<DBFiat>
+        get() = fiatsDao.getBaseFiat()
+
+    init {
+
+    }
+
 }
