b5394638601699e568b041293a5331489b519573
Jonathan Caley <jonathan.caley@ocado.com>
2020-01-05 22:25:32 +0000

    Add resources

--- app/src/main/java/com/example/cryptomanager_v2/utils/di/modules/AppModule.kt
+++ app/src/main/java/com/example/cryptomanager_v2/utils/di/modules/AppModule.kt
@@ -1,8 +1,8 @@
 package com.example.cryptomanager_v2.utils.di.modules
 
 import android.content.Context
+import android.content.res.Resources
 import com.example.cryptomanager_v2.utils.App
-import com.example.cryptomanager_v2.utils.Utils
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
@@ -14,5 +14,11 @@ open class AppModule {
     @Provides
     internal fun provideContext(application: App): Context {
         return application
+    }
+
+    @Singleton
+    @Provides
+    internal fun provideResources(context: Context): Resources {
+        return context.resources
     }
 }
