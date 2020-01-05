2678a31d269eb004c446bca10b4d74c392e1ea5c
Jonathan Caley <jonathan.caley@ocado.com>
2020-01-05 22:26:38 +0000

    Add experimental sealed classes for settings sections

--- app/src/main/java/com/example/cryptomanager_v2/ui/home/ui/settings/SettingsViewModel.kt
+++ app/src/main/java/com/example/cryptomanager_v2/ui/home/ui/settings/SettingsViewModel.kt
@@ -10,11 +10,31 @@ class SettingsViewModel @Inject constructor(
     private val fiatsDao: DBFiatsDao
 ): ViewModel() {
 
-    val fiats: LiveData<DBFiat>
+    val baseFiat: LiveData<DBFiat>
         get() = fiatsDao.getBaseFiat()
 
     init {
 
     }
-
 }
+
+//data class Settings(val settingsSections: SettingsSections)
+
+//sealed class SettingsSections {
+//    sealed class SettingsGeneral: SettingsSections() {
+//        class SelectBaseCurrency(baseFiat: DBFiat): SettingsGeneral()
+//        object NightMode: SettingsGeneral()
+//    }
+//    sealed class SettingsData: SettingsSections() {
+//        object SavedArticles: SettingsData()
+//        object TransactionHistory: SettingsData()
+//        object DeleteArticles: SettingsData()
+//        object DeletePortfolio: SettingsData()
+//    }
+//    sealed class SettingsAbout: SettingsSections() {
+//        object ReviewApp: SettingsAbout()
+//        object ShareApp: SettingsAbout()
+//        object SendFeedback: SettingsAbout()
+//        class Version(version: String): SettingsAbout()
+//    }
+//}
