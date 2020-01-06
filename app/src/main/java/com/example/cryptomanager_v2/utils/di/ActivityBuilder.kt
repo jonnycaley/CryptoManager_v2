ce3e233a21a60d8c013310d93ee6595ef5690004
Jonathan Caley <jonathan.caley@ocado.com>
2020-01-06 21:23:36 +0000

    Add provides for other activities

--- app/src/main/java/com/example/cryptomanager_v2/utils/di/ActivityBuilder.kt
+++ app/src/main/java/com/example/cryptomanager_v2/utils/di/ActivityBuilder.kt
@@ -1,6 +1,9 @@
 package com.example.cryptomanager_v2.utils.di
 
+import com.example.cryptomanager_v2.ui.home.HomeActivity
 import com.example.cryptomanager_v2.ui.splash.SplashActivity
+import com.example.cryptomanager_v2.ui.test.TestActivity
+import com.example.cryptomanager_v2.utils.di.home.HomeFragmentBuildersModule
 import com.example.cryptomanager_v2.utils.di.scopes.ActivityScope
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
@@ -11,4 +14,14 @@ abstract class ActivityBuilder {
     @ActivityScope
     @ContributesAndroidInjector
     abstract fun splashActivity(): SplashActivity
+
+    @ActivityScope
+    @ContributesAndroidInjector(
+        modules = [HomeFragmentBuildersModule::class]
+    )
+    abstract fun homeActivity(): HomeActivity
+
+    @ActivityScope
+    @ContributesAndroidInjector
+    abstract fun testActivity(): TestActivity
 }
