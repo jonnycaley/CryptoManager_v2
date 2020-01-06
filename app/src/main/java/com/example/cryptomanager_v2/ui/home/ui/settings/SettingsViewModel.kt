d76cfcf012eb813f1f91be3a148935bb26f58a85
Jonathan Caley <jonathan.caley@ocado.com>
2020-01-06 22:07:24 +0000

    Corrected logic for function

--- app/src/main/java/com/example/cryptomanager_v2/ui/home/ui/settings/SettingsViewModel.kt
+++ app/src/main/java/com/example/cryptomanager_v2/ui/home/ui/settings/SettingsViewModel.kt
@@ -4,6 +4,7 @@ import android.content.res.Resources
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.cryptomanager_v2.BuildConfig
 import com.example.cryptomanager_v2.R
 import com.example.cryptomanager_v2.data.db.fiats.DBFiat
 import com.example.cryptomanager_v2.data.db.fiats.DBFiatsDao
@@ -18,42 +19,38 @@ class SettingsViewModel @Inject constructor(
     val baseFiat: LiveData<DBFiat>
         get() = fiatsDao.getBaseFiat()
 
-    var _settingsData = MutableLiveData<SettingsData>()
+    private var _settingsData = MutableLiveData<SettingsData>()
 
     val settingsData: LiveData<SettingsData>
         get() = _settingsData
 
-    init {
-        loadSettingsSections()
-    }
-
-    private fun loadSettingsSections() {
+    fun loadSettingsSections(baseFiat: DBFiat) {
 
         val generalTitle = resources.getString(R.string.settings_fragment_general_title)
         val generalStrings = resources.getStringArray(R.array.settings_fragment_general)
         val generalItems = mutableListOf<ClickableItem>().apply {
-            add(ClickableItem(String.format(generalStrings[0], baseFiat.value?.name, baseFiat.value?.rate), println("Clicked 0!")))
-            add(ClickableItem(generalStrings[1], println("Clicked 1!")))
+            add(ClickableItem(String.format(generalStrings[0], baseFiat.name, baseFiat.rate)) {println("Clicked 0!")})
+            add(ClickableItem(generalStrings[1]) {println("Clicked 1!")})
         }
         val generalSection = Section(generalTitle, generalItems)
 
         val dataTitle = resources.getString(R.string.settings_fragment_data_title)
         val dataStrings = resources.getStringArray(R.array.settings_fragment_data)
         val dataItems = mutableListOf<ClickableItem>().apply {
-            add(ClickableItem(dataStrings[0], println("Clicked 0!")))
-            add(ClickableItem(dataStrings[1], println("Clicked 1!")))
-            add(ClickableItem(dataStrings[2], println("Clicked 2!")))
-            add(ClickableItem(dataStrings[3], println("Clicked 3!")))
+            add(ClickableItem(dataStrings[0]) {println("Clicked 0!")})
+            add(ClickableItem(dataStrings[1]) {println("Clicked 1!")})
+            add(ClickableItem(dataStrings[2]) {println("Clicked 2!")})
+            add(ClickableItem(dataStrings[3]) {println("Clicked 3!")})
         }
         val dataSection = Section(dataTitle, dataItems)
 
         val aboutTitle = resources.getString(R.string.settings_fragment_about_title)
         val aboutStrings = resources.getStringArray(R.array.settings_fragment_about)
         val aboutItems = mutableListOf<ClickableItem>().apply {
-            add(ClickableItem(aboutStrings[0], println("Clicked 0!")))
-            add(ClickableItem(aboutStrings[1], println("Clicked 1!")))
-            add(ClickableItem(aboutStrings[2], println("Clicked 2!")))
-            add(ClickableItem(aboutStrings[3], println("Clicked 3!")))
+            add(ClickableItem(aboutStrings[0]) {println("Clicked 0!")})
+            add(ClickableItem(aboutStrings[1]) {println("Clicked 1!")})
+            add(ClickableItem(aboutStrings[2]) {println("Clicked 2!")})
+            add(ClickableItem(String.format(aboutStrings[3], BuildConfig.VERSION_NAME)) {println("Clicked 3!")})
         }
         val aboutSection = Section(aboutTitle, aboutItems)
 
