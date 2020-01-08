f0b5e7d3984e17b47ad49855adf6a4578b8b5e49
Jonathan Caley <jonathan.caley@ocado.com>
2020-01-08 21:11:52 +0000

    Add divider large model

---
+++ app/src/main/java/com/example/cryptomanager_v2/ui/home/ui/settings/models/DividerLargeModel.kt
@@ -0,0 +1,14 @@
+package com.example.cryptomanager_v2.ui.home.ui.settings.models
+
+import android.view.View
+import com.airbnb.epoxy.EpoxyHolder
+import com.airbnb.epoxy.EpoxyModelClass
+import com.airbnb.epoxy.EpoxyModelWithHolder
+import com.example.cryptomanager_v2.R2
+
+@EpoxyModelClass(layout = R2.layout.divider_large)
+abstract class DividerLargeModel: EpoxyModelWithHolder<DividerLargeModel.Holder>() {
+    inner class Holder : EpoxyHolder() {
+        override fun bindView(itemView: View) {}
+    }
+}
