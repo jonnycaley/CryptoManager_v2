4b07c0f6e1324de928f1e6e88b4c5ba9c2d1ae88
Jonathan Caley <jonathan.caley@ocado.com>
2020-01-12 20:09:01 +0000

    Get replaced with Optional

--- app/src/main/java/com/example/cryptomanager_v2/data/model/CryptoCompare/exchanges/Exchange.kt
+++ app/src/main/java/com/example/cryptomanager_v2/data/model/CryptoCompare/exchanges/Exchange.kt
@@ -1,10 +1,10 @@
 package com.example.cryptomanager_v2.data.model.cryptocompare.exchanges
 
-import com.example.cryptomanager_v2.data.db.exchanges.cryptos.DBExchangeCrypto
 import com.example.cryptomanager_v2.data.db.exchanges.DBExchange
+import com.example.cryptomanager_v2.data.db.exchanges.cryptos.DBExchangeCrypto
 import org.json.JSONException
-import org.json.JSONTokener
 import org.json.JSONObject
+import org.json.JSONTokener
 
 class Exchange {
     companion object {
@@ -18,17 +18,19 @@ class Exchange {
 
                     while (keys.hasNext()) {
                         val exchange = keys.next()
-                        val innerJsonObject = jsonObject.getJSONObject(exchange)
-                        val baseCryptos = innerJsonObject.keys()
+                        val innerJsonObject = jsonObject.optJSONObject(exchange)
+                        val baseCryptos = innerJsonObject?.keys()
                         val cryptoAndConversion = arrayListOf<Pair<String, List<String>>>()
-                        while (baseCryptos.hasNext()) {
+                        while (baseCryptos != null && baseCryptos.hasNext()) {
                             val baseCrypto = baseCryptos.next()
-                            val conversionCryptos = innerJsonObject.getJSONArray(baseCrypto)
+                            val conversionCryptos = innerJsonObject.optJSONArray(baseCrypto)
 
-                            val conversionCryptosString = (0 until conversionCryptos.length())
-                                .map { conversionCryptos.get(it) as String }
+                            conversionCryptos?.let { cryptos ->
+                                val conversionCryptosString = (0 until cryptos.length())
+                                    .map { conversionCryptos.opt(it) as String }
 
-                            cryptoAndConversion.add(Pair(baseCrypto, conversionCryptosString))
+                                cryptoAndConversion.add(Pair(baseCrypto, conversionCryptosString))
+                            }
                         }
                         add(DBExchange(exchange,
                             DBExchangeCrypto(
