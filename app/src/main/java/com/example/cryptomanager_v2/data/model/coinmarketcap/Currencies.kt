b4bb51dd30ca2cad4754f2359d5ed441db2c94d0
Jonathan Caley <jonathan.caley@ocado.com>
2020-01-13 20:45:06 +0000

    Add coinmarket cap currencies

---
+++ app/src/main/java/com/example/cryptomanager_v2/data/model/coinmarketcap/Currencies.kt
@@ -0,0 +1,111 @@
+package com.example.cryptomanager_v2.data.model.coinmarketcap
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
+class Currencies : Serializable {
+
+    @SerializedName("status")
+    @Expose
+    var status: Status? = null
+    @SerializedName("data")
+    @Expose
+    var data: List<Currency>? = null
+}
+
+class Status : Serializable {
+
+    @SerializedName("timestamp")
+    @Expose
+    var timestamp: String? = null
+    @SerializedName("error_code")
+    @Expose
+    var errorCode: Int? = null
+    @SerializedName("error_message")
+    @Expose
+    var errorMessage: Any? = null
+    @SerializedName("elapsed")
+    @Expose
+    var elapsed: Int? = null
+    @SerializedName("credit_count")
+    @Expose
+    var creditCount: Int? = null
+
+    companion object {
+        private const val serialVersionUID = 3375820567581602557L
+    }
+}
+
+
+class Currency : Serializable {
+
+    @SerializedName("id")
+    var id: Long? = null
+
+    @SerializedName("name")
+    var name: String? = null
+
+    @SerializedName("symbol")
+    var symbol: String? = null
+
+    @SerializedName("slug")
+    var slug: String? = null
+
+    @SerializedName("circulating_supply")
+    var circulatingSupply: Float? = null
+
+    @SerializedName("total_supply")
+    var totalSupply: Float? = null
+
+    @SerializedName("max_supply")
+    var maxSupply: Float? = null
+
+    @SerializedName("date_added")
+    var dateAdded: String? = null
+
+    @SerializedName("num_market_pairs")
+    var numMarketPairs: Long? = null
+
+    @SerializedName("cmc_rank")
+    var cmcRank: Long? = null
+
+    @SerializedName("last_updated")
+    var lastUpdated: String? = null
+
+    @SerializedName("quote")
+    var quote: Quote? = null
+}
+
+class Quote : Serializable {
+
+    @SerializedName("USD")
+    var uSD: USD? = null
+
+    @SerializedName("BTC")
+    var bTC: USD? = null
+}
+
+class USD : Serializable {
+
+    @SerializedName("price")
+    var price: Float? = null
+
+    @SerializedName("volume_24h")
+    var volume24h: Float? = null
+
+    @SerializedName("percent_change_1h")
+    var percentChange1h: Float? = null
+
+    @SerializedName("percent_change_24h")
+    var percentChange24h: Float? = null
+
+    @SerializedName("percent_change_7d")
+    var percentChange7d: Float? = null
+
+    @SerializedName("market_cap")
+    var marketCap: Float? = null
+
+    @SerializedName("last_updated")
+    var lastUpdated: String? = null
+}
--- app/src/test/java/com/example/cryptomanager_v2/utils/json/JsonUtils.kt
+++ app/src/test/java/com/example/cryptomanager_v2/utils/json/JsonUtils.kt
@@ -2,6 +2,8 @@ package com.example.cryptomanager_v2.utils.json
 
 import android.content.Context
 
-private fun readJsonFile(context: Context, filePath: String): String {
-    return context.javaClass.classLoader?.getResource(filePath)?.readText().toString()
+object JsonUtils {
+    private fun readJsonFile(context: Context, filePath: String): String {
+        return context.javaClass.classLoader?.getResource(filePath)?.readText().toString()
+    }
 }
