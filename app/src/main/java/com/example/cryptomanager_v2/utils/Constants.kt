9089570765c91a446640944e1b29b9cc76804911
Jonathan Caley <jonathan.caley@ocado.com>
2020-01-13 20:45:46 +0000

    Add coinmarketcap api credentials

--- app/src/main/java/com/example/cryptomanager_v2/utils/Constants.kt
+++ app/src/main/java/com/example/cryptomanager_v2/utils/Constants.kt
@@ -7,4 +7,8 @@ object Constants {
     val URL_CRYPTOCOMPARE = "https://min-api.cryptocompare.com/data/"
     val NAME_CRYPTOCOMPARE = "api_key"
     val KEY_CRYPTOCOMPARE = "a5df989c3d35841e7434d865f1a818d6b9de1491aae9895022c984833bbfcf00"
+
+    val URL_COINMARKETCAP = "https://pro-api.coinmarketcap.com/v1/"
+    val NAME_COINMARKETCAP = "CMC_PRO_API_KEY"
+    val KEY_COINMARKETCAP = "a17fe7b8-33e3-4a80-a1ee-7e4ffce2078f"
 }
