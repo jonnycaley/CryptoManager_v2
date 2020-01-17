19cb4bcf68caab4455a0b42deb0ddd34be7ada01
Jonathan Caley <jonathan.caley@ocado.com>
2020-01-17 19:25:28 +0000

    Cleanup fragments lifecycle

--- app/src/main/java/com/example/cryptomanager_v2/ui/home/ui/markets/MarketsFragment.kt
+++ app/src/main/java/com/example/cryptomanager_v2/ui/home/ui/markets/MarketsFragment.kt
@@ -4,22 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.example.cryptomanager_v2.R
+import javax.inject.Inject
 
 class MarketsFragment : Fragment() {
-    private lateinit var marketsViewModel: MarketsViewModel
+    @Inject
+    lateinit var marketsViewModel: MarketsViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        marketsViewModel
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        marketsViewModel =
-            ViewModelProviders.of(this).get(MarketsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_markets, container, false)
-        val textView: TextView = root.findViewById(R.id.text_markets)
-        marketsViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
-        return root
+        return inflater.inflate(R.layout.fragment_markets, container, false)
     }
 }
