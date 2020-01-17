088ef85f780ff0bac403e09512ed6056cdd0ebd4
Jonathan Caley <jonathan.caley@ocado.com>
2020-01-17 19:24:41 +0000

    Change api

--- app/src/main/java/com/example/cryptomanager_v2/data/network/coinmarketcap/CoinMarketCapApi.kt
+++ app/src/main/java/com/example/cryptomanager_v2/data/network/coinmarketcap/CoinMarketCapApi.kt
@@ -3,10 +3,9 @@ package com.example.cryptomanager_v2.data.network.coinmarketcap
 import com.example.cryptomanager_v2.data.model.coinmarketcap.Currencies
 import io.reactivex.Observable
 import retrofit2.http.GET
-import retrofit2.http.Query
 
 interface CoinMarketCapApi {
 
-    @GET("cryptocurrency/listings/latest?start=1&convert=USD")
-    suspend fun getTopUSD(@Query("limit") symbol: String): Observable<Currencies>
+    @GET("cryptocurrency/listings/latest?start=1&convert=USD&limit=100")
+    fun getTopUSD(): Observable<Currencies>
 }
