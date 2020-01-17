96bd8d630140f37e4ae25ac07f8709e4a5ab3120
Jonathan Caley <jonathan.caley@ocado.com>
2020-01-17 19:25:49 +0000

    Get top 100

--- app/src/main/java/com/example/cryptomanager_v2/ui/home/ui/markets/MarketsViewModel.kt
+++ app/src/main/java/com/example/cryptomanager_v2/ui/home/ui/markets/MarketsViewModel.kt
@@ -1,12 +1,33 @@
 package com.example.cryptomanager_v2.ui.home.ui.markets
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.cryptomanager_v2.data.model.coinmarketcap.Currencies
+import com.example.cryptomanager_v2.data.network.coinmarketcap.CoinMarketCapService
+import com.example.cryptomanager_v2.utils.AppSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
+import javax.inject.Inject
 
-class MarketsViewModel : ViewModel() {
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is markets Fragment"
+class MarketsViewModel @Inject constructor(
+    private val coinMarketCapService: CoinMarketCapService,
+    private val appSchedulers: AppSchedulers
+): ViewModel() {
+
+    private val compositeDisposable = CompositeDisposable()
+
+    init {
+        getCryptos()
     }
-    val text: LiveData<String> = _text
+
+    private fun getCryptos() {
+        coinMarketCapService.getTop100()
+            .subscribeOn(appSchedulers.io)
+            .observeOn(appSchedulers.mainThread)
+            .subscribe(::showTop100)
+            .addTo(compositeDisposable)
+    }
+
+    private fun showTop100(top100: Currencies){
+
+    }
 }
