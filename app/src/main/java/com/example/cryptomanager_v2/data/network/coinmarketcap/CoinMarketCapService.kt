e34118417dd21adaaa698a7e280c94df1a88282e
Jonathan Caley <jonathan.caley@ocado.com>
2020-01-18 14:42:15 +0000

    Add coroutine logic to service

--- app/src/main/java/com/example/cryptomanager_v2/data/network/coinmarketcap/CoinMarketCapService.kt
+++ app/src/main/java/com/example/cryptomanager_v2/data/network/coinmarketcap/CoinMarketCapService.kt
@@ -1,14 +1,21 @@
 package com.example.cryptomanager_v2.data.network.coinmarketcap
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.liveData
 import com.example.cryptomanager_v2.data.model.coinmarketcap.Currencies
-import com.example.cryptomanager_v2.utils.AppSchedulers
+import com.example.cryptomanager_v2.utils.Resource
 import javax.inject.Inject
 
 class CoinMarketCapService @Inject constructor(
-    private val schedulers: AppSchedulers,
     private val coinMarketCapApi: CoinMarketCapApi
 ){
-    suspend fun getTop100(): Currencies {
-        return coinMarketCapApi.getTopUSD()
+    fun getTop100(): LiveData<Resource<Currencies>> = liveData {
+        emit(Resource.loading())
+        try {
+            val data = coinMarketCapApi.getTopUSD()
+            emit(Resource.success(data))
+        } catch (e: Exception) {
+            emit(Resource.error(e.message ?: "An error occurred"))
+        }
     }
 }
