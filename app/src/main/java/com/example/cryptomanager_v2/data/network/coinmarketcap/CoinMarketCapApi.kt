646c0a23ea50a1273b9884660358565f654f50f7
Jonathan Caley <jonathan.caley@ocado.com>
2020-01-18 14:19:56 +0000

    Remove observable for coroutines

--- app/src/main/java/com/example/cryptomanager_v2/data/network/coinmarketcap/CoinMarketCapApi.kt
+++ app/src/main/java/com/example/cryptomanager_v2/data/network/coinmarketcap/CoinMarketCapApi.kt
@@ -1,11 +1,10 @@
 package com.example.cryptomanager_v2.data.network.coinmarketcap
 
 import com.example.cryptomanager_v2.data.model.coinmarketcap.Currencies
-import io.reactivex.Observable
 import retrofit2.http.GET
 
 interface CoinMarketCapApi {
 
     @GET("cryptocurrency/listings/latest?start=1&convert=USD&limit=100")
-    fun getTopUSD(): Observable<Currencies>
+    suspend fun getTopUSD(): Currencies
 }
--- app/src/main/java/com/example/cryptomanager_v2/data/network/coinmarketcap/CoinMarketCapService.kt
+++ app/src/main/java/com/example/cryptomanager_v2/data/network/coinmarketcap/CoinMarketCapService.kt
@@ -2,14 +2,13 @@ package com.example.cryptomanager_v2.data.network.coinmarketcap
 
 import com.example.cryptomanager_v2.data.model.coinmarketcap.Currencies
 import com.example.cryptomanager_v2.utils.AppSchedulers
-import io.reactivex.Observable
 import javax.inject.Inject
 
 class CoinMarketCapService @Inject constructor(
     private val schedulers: AppSchedulers,
     private val coinMarketCapApi: CoinMarketCapApi
 ){
-    fun getTop100(): Observable<Currencies> {
+    suspend fun getTop100(): Currencies {
         return coinMarketCapApi.getTopUSD()
     }
 }
