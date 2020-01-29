c12cfe59cb33bf7c13f0d9cbd8d6e2dd0ba2d91d
Jonathan Caley <jonathan.caley@ocado.com>
2020-01-29 22:16:15 +0000

    Livedata extensions to fix mediatorlivedata nullability https://proandroiddev.com/nonnull-livedata-with-kotlin-extension-26963ffd0333

--- app/src/main/java/com/example/cryptomanager_v2/utils/Extensions.kt
+++ app/src/main/java/com/example/cryptomanager_v2/utils/Extensions.kt
@@ -1,4 +1,22 @@
 package com.example.cryptomanager_v2.utils
 
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.Observer
+
 val <T> T.exhaustive: T
     get() = this
+
+class NonNullMediatorLiveData<T> : MediatorLiveData<T>()
+
+fun <T> LiveData<T>.nonNull(): NonNullMediatorLiveData<T> {
+    val mediator: NonNullMediatorLiveData<T> = NonNullMediatorLiveData()
+    mediator.addSource(this, Observer { it?.let { mediator.value = it } })
+    return mediator
+}
+fun <T> NonNullMediatorLiveData<T>.observe(owner: LifecycleOwner, observer: (t: T) -> Unit) {
+    this.observe(owner, Observer {
+        it?.let(observer)
+    })
+}
